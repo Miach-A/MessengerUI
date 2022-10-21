f82fe7a8ff5300707e3ef54ae5a3e1d55dd7f6f7
Miach-A <ovmiach@gmail.com>
2022-10-21 11:36:45 +0300

    save

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
 
 import { AngularMaterialModule } from './modules/angular-material.module';
+import { AUTH_API_URL, MESSENGER_API_URL } from './app-injection-tokens';
+import { environment } from 'src/environments/environment';
+import { JwtModule } from '@auth0/angular-jwt';
+import { ACCES_TOKEN_KEY } from './services/auth.service';
 
+export function tokenGetter(){
+  return localStorage.getItem(ACCES_TOKEN_KEY);
+}
 @NgModule({
   declarations: [
     AppComponent
@@ -17,9 +25,22 @@ import { AngularMaterialModule } from './modules/angular-material.module';
     AppRoutingModule,
     AngularMaterialModule,
     BrowserAnimationsModule,
-    FormsModule
+    FormsModule,
+    HttpClientModule,
+    JwtModule . forRoot ({
+      config : {
+        tokenGetter : tokenGetter,
+        allowedDomains: [environment.messengerApi]
+      } })
   ],
-  providers: [],
+  providers: [{
+    provide:AUTH_API_URL,
+    useValue:environment.authApi
+  },
+  {
+    provide:MESSENGER_API_URL,
+    useValue:environment.messengerApi
+  }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
