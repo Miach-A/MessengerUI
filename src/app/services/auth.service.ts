e634e123b7f4532f358103816e4c665a1ae5e93e
Miach-A <ovmiach@gmail.com>
2022-10-21 10:34:05 +0300

    save

---
+++ src/app/services/auth.service.ts
@@ -0,0 +1,39 @@
+import { Inject, Injectable } from '@angular/core';
+import { User } from '../models/User';
+import { HttpClient } from '@angular/common/http';
+import { AUTH_API_URL } from '../app-injection-tokens';
+import { JwtHelperService } from '@auth0/angular-jwt';
+import { Token } from '../models/Token';
+import { Observable } from 'rxjs';
+import { Router } from '@angular/router';
+
+export const ACCES_TOKEN_KEY = "chat_access_token"
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+  private _user:User = new User();
+  constructor(private httpClient:HttpClient,
+    @Inject(AUTH_API_URL) private apiUrl:string,
+    private JwtHelper:JwtHelperService,
+    private router:Router) {   }
+
+  login(name:string, password:string):Observable<Token>{
+    return this.httpClient.post<Token>(`${this.apiUrl}api/Authenticate`, {name: name, password:password});
+  }
+
+  isAuthenticated():boolean{
+    var token = localStorage.getItem(ACCES_TOKEN_KEY);
+    return !!token && !this.JwtHelper.isTokenExpired(token);
+  }
+
+  logout():void{
+    localStorage.removeItem(ACCES_TOKEN_KEY);
+    this.router.navigate(['']);
+  }
+
+  CurentUser():User{
+    return this._user;
+  }
+}
