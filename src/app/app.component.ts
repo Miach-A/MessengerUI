f730f0bbb54eea0032f85176dc14091e767d9fa8
Miach-A <ovmiach@gmail.com>
2022-10-22 12:37:52 +0300

    save

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,6 +4,7 @@ import { JwtHelperService } from '@auth0/angular-jwt';
 import { Subscription } from 'rxjs';
 import { User } from './models/User';
 import { ACCES_TOKEN_KEY, AuthService } from './services/auth.service';
+import { BackendService } from './services/backend.service';
 
 @Component({
   selector: 'app-root',
@@ -16,21 +17,22 @@ export class AppComponent {
   breakpointSubscribe!:Subscription;
   modeSide:boolean = false;
   subscriptions: Subscription[] = [];
-  token:string = "";
+  //token:string = "";
   public user: User = new User();
 
   constructor(private breakpointObserver:BreakpointObserver,
     public authService: AuthService,
+    private backendService:BackendService,
     //public signalrService: SignalrService,
     private JwtHelper: JwtHelperService) {
 
   }
   ngOnInit(): void {
     this.breakpointSubscribe = this.breakpointObserver.observe(["(min-width:1024px)"]).subscribe(x => this.modeSide = x.matches);
-    var token = localStorage.getItem(ACCES_TOKEN_KEY);
+    /*var token = localStorage.getItem(ACCES_TOKEN_KEY);
     if (token != null){
       this.token = token;
-    } 
+    }  */
   }
 
   ngOnDestroy():void{
@@ -41,17 +43,33 @@ export class AppComponent {
     var sub = this.authService.login(name,password).subscribe({
       next: (token) => {
       localStorage.setItem(ACCES_TOKEN_KEY, token.access_token);
-      this.token = token.access_token;
+      //this.token = token.access_token;
       //var decodeToken = this.JwtHelper.decodeToken(token.access_token);
       //this.user.name = decodeToken.name;
       //this.user = decodeToken.sub;
       //this.signalrService.user = this.user;
       //this.signalrService.Connect(); 
+
+       var subUser = this.backendService.get("User").subscribe({
+        next: (user) => {
+          this.user = user as User;
+          console.log(this.user);
+        },
+        error: (er) => {
+          alert(er.message);
+          //console.log(er);
+          //this.authService.logout();
+        }
+      });
+      this.subscriptions.push(subUser);
+
     },
     error: (error) => {
       alert('Login error');
     }
   });
+
+
     this.subscriptions.push(sub);
   }
 
--- src/app/services/backend.service.ts
+++ src/app/services/backend.service.ts
@@ -2,32 +2,49 @@ import { Inject, Injectable } from '@angular/core';
 import { BACKEND_API_URL } from '../app-injection-tokens';
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { observable, Observable, of } from 'rxjs';
+import { ACCES_TOKEN_KEY } from './auth.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class BackendService {
 
-  constructor(private httpClient : HttpClient,
+  constructor(private httpClient: HttpClient,
     @Inject(BACKEND_API_URL) private uriBase: string) { }
 
-    public get(uri:string,guid?:string, search?:{[key:string]:string} ):Observable<Object>{
-      if (uri === undefined){
-        return of({});
-      }
+  public get(uri: string | undefined, guid?: string, search?: { [key: string]: string }): Observable<Object> {
+    if (uri === undefined) {
+      return of({});
+    }
 
-      var params = new HttpParams();
-      if (search!==null){
-        for (const key in search) {
-           params = params.append(key,search[key]);
-          }
-        }
-  
-      if(guid != undefined && guid != "" && guid != null){
-        return this.httpClient.get(this.uriBase.concat(uri).concat("/"+guid),{params:params});
+    var params = new HttpParams();
+    if (search !== null) {
+      for (const key in search) {
+        params = params.append(key, search[key]);
       }
+    }
 
-      return this.httpClient.get(this.uriBase.concat(uri),{params:params});
-     }
-  
+    if (guid != undefined && guid != "" && guid != null) {
+      return this.httpClient.get(this.uriBase.concat(uri).concat("/" + guid), { params: params });
+    }
+
+    return this.httpClient.get(this.uriBase.concat(uri), { params: params });
+  }
+
+  public post(uri: string | undefined, entity: any) {
+    if (uri === undefined || uri === "") {
+      return of({});
+    }
+
+    return this.httpClient.post(this.uriBase.concat(uri), entity);
+  }
+
+  public put(uri: string, guid: string, entity: any) {
+    if (uri === undefined) {
+      return of({});
+    }
+
+    return this.httpClient.put(this.uriBase.concat(uri + "/" + guid), entity);
+  }
+
 }
