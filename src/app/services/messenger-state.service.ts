dbfbe2ec7acce6e76f14ebbbebe40f01ca8f5c3c
Miach-A <ovmiach@gmail.com>
2022-10-27 20:24:09 +0300

    save

--- src/app/services/messenger-state.service.ts
+++ src/app/services/messenger-state.service.ts
@@ -8,9 +8,49 @@ import { User } from '../models/User';
   providedIn: 'root'
 })
 export class MessengerStateService {
-  private _user:User = new User();
-  private _chat:Chat = new Chat();
+  private _user?:User;
+  private _chat?:Chat;
   private _event:ChatEvent = ChatEvent.New;
-  private _commentedMessage:Message = new Message();
+  private _targetMessage?:Message;
+  private _targetChat?:Chat;
   constructor() { }
+
+  public SetUser(user:User){
+    this._user = user; 
+    this._chat = undefined;
+    this._event = ChatEvent.New;
+    this._targetMessage = undefined;
+  }
+
+  public GetUser():User|undefined{
+    return this._user;
+  }
+
+  public SetChat(chat:Chat){
+    this._chat = chat;
+    this._event = ChatEvent.New;
+    this._targetMessage = undefined;
+    this._targetChat = undefined;
+  }
+
+  public SetTargetChat(chat:Chat,message:Message){
+    this._targetChat = chat;
+    this._event = ChatEvent.Comment;
+    this._targetMessage = message;
+  }
+
+  public GetChat():Chat|undefined{
+    return this._targetChat === undefined ? this._chat : this._targetChat;
+  }
+
+  public BeginComment(message:Message){
+    this._event = ChatEvent.Comment;
+    this._targetMessage = message;
+  }
+
+  public BeginEdit(message:Message){
+    this._event = ChatEvent.Edit;
+    this._targetMessage = message;
+  }
+
 }
