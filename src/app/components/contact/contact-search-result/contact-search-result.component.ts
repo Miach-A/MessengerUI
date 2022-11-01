8ca3571638743451cfcb2bb571d98d72ac8d6fc9
Miach-A <ovmiach@gmail.com>
2022-11-01 13:57:46 +0200

    save

--- src/app/components/contact/contact-search-result/contact-search-result.component.ts
+++ src/app/components/contact/contact-search-result/contact-search-result.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
-import { Subscription } from 'rxjs';
+import { Subscription, switchMap, tap } from 'rxjs';
 import { BackendService } from 'src/app/services/backend.service';
 import { MessengerStateService } from 'src/app/services/messenger-state.service';
 
@@ -10,16 +10,26 @@ import { MessengerStateService } from 'src/app/services/messenger-state.service'
 })
 export class ContactSearchResultComponent implements OnInit, OnDestroy {
   private _subscriptions:Subscription[] = [];
+  private _searchForm:any;
   
   constructor(
     private messengerState:MessengerStateService,
     private backendService:BackendService
   ) { 
-    this._subscriptions.push(
-      this.messengerState.getContactSearchEmitter().subscribe({
+/*     this._subscriptions.push(
+      this.messengerState.getContactSearchEmitter()
+      .subscribe({
         next: (data:any) => this.ContactSearchChange(data)
       })
-    );
+    ); */
+    this._subscriptions.push(
+    this.messengerState.getContactSearchEmitter()
+    .pipe(
+      tap((data:any) => this._searchForm = data),
+      switchMap((data:any) => this.backendService.get("GetUsers", undefined, data))
+    ).subscribe({
+      next: (data) => { console.log(data); }
+    }));
   }
 
   ngOnDestroy(): void {
@@ -32,11 +42,11 @@ export class ContactSearchResultComponent implements OnInit, OnDestroy {
 
   }
 
-  ContactSearchChange(data:any){
+/*   ContactSearchChange(data:any){
     this._subscriptions.push(
       this.backendService.get("GetUsers", undefined, data).subscribe({
         next: (data) => { console.log(data); }
       }));
-  }
+  } */
 
 }
