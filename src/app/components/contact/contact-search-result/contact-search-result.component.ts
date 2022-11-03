53f378cefdbaa5c20a3c2c4c0686481b73dcde83
Miach-A <ovmiach@gmail.com>
2022-11-03 10:08:05 +0200

    save

--- src/app/components/contact/contact-search-result/contact-search-result.component.ts
+++ src/app/components/contact/contact-search-result/contact-search-result.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { PageEvent } from '@angular/material/paginator';
-import { Subscription, switchMap, tap } from 'rxjs';
+import { Observable, Subscription, switchMap, tap } from 'rxjs';
 import { Contact } from 'src/app/models/Contact';
 import { BackendService } from 'src/app/services/backend.service';
 import { MessengerStateService } from 'src/app/services/messenger-state.service';
@@ -14,11 +14,13 @@ export class ContactSearchResultComponent implements OnInit, OnDestroy {
   private _subscriptions:Subscription[] = [];
   private _searchForm:any;
   private _totalCount:number = 0;
+  private _page:number = 0;
+  private _pageSize:number = 0;
   public contacts:Contact[] = [];
-  public length = 100;
+/*   public length = 100;
   public pageSize = 25;
   public pageSizeOptions: number[] = [5, 25, 50, 100];
-  public pageEvent: PageEvent = new PageEvent();
+  public pageEvent: PageEvent = new PageEvent(); */
   
   constructor(
     private messengerState: MessengerStateService,
@@ -27,17 +29,43 @@ export class ContactSearchResultComponent implements OnInit, OnDestroy {
     this._subscriptions.push(
       this.messengerState.GetContactSearchEmitter()
         .pipe(
-          tap((data: any) => this._searchForm = data),
-          switchMap((data: any) => this.backendService.get("GetUsers", undefined, data))
+          tap((data: any) => {
+            this._searchForm = data;
+            this._pageSize = this._searchForm.pagesize;
+            this._page = this._searchForm.pageindex;
+          }),
+          switchMap((data: any) => this.SearchData()) //this.backendService.get("GetUsers", undefined, data)
         ).subscribe({
-          next: (data) => {this.SetData(data);}
+          next: (data) => { this.SetData(data); }
         }));
   }
 
-  ngOnDestroy(): void {
-    this._subscriptions.forEach(subscription => {
-      subscription.unsubscribe();
-    });
+  ngOnInit(): void {
+
+  }
+
+  SearchData():Observable<Object>{
+    return this.backendService.get("GetUsers", undefined, this._searchForm);
+  }
+
+  GetPageCount():number{
+    return this._totalCount / this._pageSize + 1;
+  }
+
+  NextPage(){
+    if (this._searchForm.pageindex = this.GetPageCount()){
+      return;
+    }
+
+    this._searchForm.pageindex += 1; 
+  }
+
+  PreviousPage(){
+    if (this._searchForm.pageindex === 0){
+      return;
+    }
+
+    this._searchForm.pageindex -= 1; 
   }
 
   SetData(data:any){
@@ -48,7 +76,10 @@ export class ContactSearchResultComponent implements OnInit, OnDestroy {
     });
   }
 
-  ngOnInit(): void {
-
+  ngOnDestroy(): void {
+    this._subscriptions.forEach(subscription => {
+      subscription.unsubscribe();
+    });
   }
+
 }
