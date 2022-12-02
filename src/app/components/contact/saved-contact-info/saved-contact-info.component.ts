829771145978b92634f4138fcd18b8be85f554a2
Miach-A <ovmiach@gmail.com>
2022-12-02 10:13:40 +0200

    save

--- src/app/components/contact/saved-contact-info/saved-contact-info.component.ts
+++ src/app/components/contact/saved-contact-info/saved-contact-info.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit,OnDestroy } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { Subscription } from 'rxjs';
 import { Contact } from 'src/app/models/Contact';
@@ -12,23 +12,23 @@ import { MessengerStateService } from 'src/app/services/messenger-state.service'
   templateUrl: './saved-contact-info.component.html',
   styleUrls: ['./saved-contact-info.component.scss']
 })
-export class SavedContactInfoComponent implements OnInit,OnDestroy {
-  private _subscriptions:Subscription[] = [];
-  public contact?:Contact;
-  public saved:boolean = false;
-  public isMe:boolean = false;
+export class SavedContactInfoComponent implements OnInit, OnDestroy {
+  private _subscriptions: Subscription[] = [];
+  public contact?: Contact;
+  public saved: boolean = false;
+  public isMe: boolean = false;
 
   constructor(
-    private messengerState:MessengerStateService,
-    private activatedRoute:ActivatedRoute,
-    private backendService:BackendService,
-    private route:Router,
-    private chatService:ChatService) {
+    private messengerState: MessengerStateService,
+    private activatedRoute: ActivatedRoute,
+    private backendService: BackendService,
+    private route: Router,
+    private chatService: ChatService) {
 
-   }
+  }
 
   ngOnInit(): void {
-    this._subscriptions.push(      
+    this._subscriptions.push(
       this.activatedRoute.paramMap.subscribe({
         next: (param) => {
           this.UpdateData(param.get('contactname') ?? "");
@@ -40,18 +40,19 @@ export class SavedContactInfoComponent implements OnInit,OnDestroy {
         .subscribe({
           next: () => {
             const routeSnapShot = this.activatedRoute.snapshot;
-            this.UpdateData(routeSnapShot.paramMap.get('contactname') ?? ""); }
+            this.UpdateData(routeSnapShot.paramMap.get('contactname') ?? "");
+          }
         }));
   }
 
-  ContactSaved(){
+  ContactSaved() {
     const user = this.messengerState.GetUser();
-    if (user === undefined){
+    if (user === undefined) {
       this.saved = false;
       return;
     }
 
-    if (!!user.contacts.find(x => x.name === this.contact?.name) || user.name === this.contact?.name){
+    if (!!user.contacts.find(x => x.name === this.contact?.name) || user.name === this.contact?.name) {
       this.saved = true;
       return;
     }
@@ -60,82 +61,83 @@ export class SavedContactInfoComponent implements OnInit,OnDestroy {
   }
 
   SaveContact() {
-    if (this.contact === undefined){
+    if (this.contact === undefined) {
       return;
     }
 
     this._subscriptions.push(
       this.backendService
-        .post("PostContact", new CreateContactDTO(this.contact.name ))
+        .post("PostContact", new CreateContactDTO(this.contact.name))
         .subscribe({
-          next: (contact) => { this.messengerState.AddContact(new Contact(contact as Contact)); this.saved = true; }
+          next: (contact) => { 
+            this.contact = new Contact(contact as Contact);
+            this.messengerState.AddContact(this.contact); 
+            this.saved = true; }
         }));
   }
 
-  UpdateData(name:string){
+  UpdateData(name: string) {
     this.contact = this.messengerState.GetContact(name);
-    
-    if (this.contact != undefined){
+
+    if (this.contact != undefined) {
       this.saved = true;
     }
-    else
-    {
+    else {
       this.saved = false;
       const chat = this.messengerState.GetCurrentChat();
-      if (chat != undefined){
-        this.contact = this.messengerState.GetContact(name,chat.guid);
-      } 
+      if (chat != undefined) {
+        this.contact = this.messengerState.GetContact(name, chat.guid);
+      }
     }
 
-    //this.ContactSaved();
     this.IsMe();
   }
 
-  IsMe(){
+  IsMe() {
     this.isMe = false;
-    if(this.contact?.name === this.messengerState.GetUser()?.name){
+    if (this.contact?.name === this.messengerState.GetUser()?.name) {
       this.isMe = true;
     }
   }
 
-  DeleteContact(){
+  DeleteContact() {
     this._subscriptions.push(
       this.backendService
-        .delete("DeleteContact",this.contact?.name)
+        .delete("DeleteContact", this.contact?.name)
         .subscribe({
           next: () => {
             this.messengerState.DeleteContact(this.contact as Contact);
             //this.UpdateData(this.contact?.name ?? "");
-            this.route.navigate(['./'],{relativeTo:this.activatedRoute.parent});
+            this.route.navigate(['./'], { relativeTo: this.activatedRoute.parent });
           }
         }));
   }
 
-  OpenChat(){
+  OpenChat() {
     const user = this.messengerState.GetUser();
-    if (!user || !this.contact){
+    if (!user || !this.contact) {
       return;
     }
 
     const chat = user.chats.find(x => x.users.length === 2 && x.users.find(y => y.name === this.contact!.name));
-    if (!chat){
+    if (!chat) {
       const contacts = new Array<string>();
-      if (this.contact?.name !== undefined){
+      if (this.contact?.name !== undefined) {
         contacts.push(this.contact?.name);
       }
       this.chatService.CreateChat(contacts);
       return;
     }
 
-    this.route.navigate(['chat',chat.guid]);
+    this.route.navigate(['chat', chat.guid]);
   }
 
-  SavedContact(){
+  SavedContact() {
     return !!this.messengerState.GetUser()?.contacts.find(x => x === this.contact);
   }
 
-  ParentRoute(){
-    this.route.navigate(['./'],{relativeTo:this.activatedRoute.parent});
+  ParentRoute() {
+    this.route.navigate(['./'], { relativeTo: this.activatedRoute.parent });
   }
 
   ngOnDestroy(): void {
